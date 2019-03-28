c7273c75367ca2ba64dcafda4ef70800a5c19d49
AnthonyBlackJ <1303524991@qq.com>
2019-03-28 09:36:59 -0700

    增加插入排序

--- 01线性表/01单链表.py
+++ 01线性表/01单链表.py
@@ -83,7 +83,64 @@ class SingLinkList(object):
 				return False
 		return True
 		
+	def rev(self):
+		'''链表反转'''
+		'''将一个链表的首节点不断取下，将其插入到另一个链表'''
+		p = None
+		if self._head is None:
+			return '''列表weikong'''
+		while self._head is not None:
+			q = self._head
+			self._head = q.next_
+			q.next_ = p
+			p = q
+		self._head = p
 
+	def sort(self):
+		'''插入排序(移动元素)'''
+		if self._head is None:
+			return '''链表为空，无法排序'''
+		# 用于标记正在排序的节点,从第二个开始
+		p = self._head.next_
+		while p is not None:
+			# 用于对前面有序段。寻找一个正确的位置
+			q = self._head
+			x = p.elem
+			while q != p and q.elem <= x:
+				q = q.next_
+			while q != p:
+				y = q.elem
+				q.elem = x
+				x = y
+				q = q.next_
+			p.elem = x	
+			p = p.next_
+			 
+	def sort2(self):
+		'''插入排序(更改链接)'''
+		'''实际处理为一个个取下链表节点，将其插入到一段元素递增的节点链中正确的位置中'''
+		p = self._head
+		# 只有一个元素或为空，无需排序
+		if p is None and p.next_ is None:
+			return 
+		
+		rem = p.next_ # 排序从第二开始
+		p.next_ = None # 将有序部分的末尾设置
+		while rem is not None:
+			i = self._head
+			j = None
+			# 有序部分的遍历
+			while i != None and i.elem <= rem.elem:
+				j = i
+				i = i.next_
+			# 是否在表头插入
+			if j is None:
+				self._head = rem
+			else:
+				j.next_ = rem
+			j = rem
+			rem = rem.next_
+			j.next_ = i
 	
 def main():
 	sll = SingLinkList()
@@ -110,6 +167,12 @@ def main():
 	sll.travell()
 	print("**********")
 	print(sll.length())
+	sll.rev()
+	sll.travell()
+	print("**********")
+	#sll.sort()
+	sll.sort2()
+	sll.travell()
 
 if __name__ == "__main__":
 	main()
